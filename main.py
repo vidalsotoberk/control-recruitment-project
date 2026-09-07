19f24bab6d3aab920a52be7e68bc06bd4a0375ed
vidalsotoberk <vidalsoto0831@berkeley.edu>
2026-09-07 14:31:01 -0700

    added notes from office hours. I understand the general goal a little more and how to get there. Couple lines of code added.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline
+import math # for pure pursuit?
 from simulator import Simulator, centerline
 
 sim = Simulator()
+
+def reverse_centerline(x, y):
+    """
+    takes in some x and y coordinate and returns the closest centerline position
+    """
 
 
 
@@ -16,16 +24,41 @@ def controller(x):
     """
     xpos   = x[0]                   # current x position
     ypos   = x[1]                   # current y position
-    phi    = np.mod(x[2], 2*np.pi)  # current heading (radians)
+    phi    = np.mod(x[2], 2*np.pi)  # current heading (radians) (direction of car?)
     v      = x[3]                   # current velocity
     theta   = x[4]                  # current steering angle
 
-    
-    
     ... # YOUR CODE HERE
+    """
+    BIG IDEA: need to check where the car is and how fast it going, and then determine
+    how the car should accelerate and how much to turn the steering wheel
 
-    #test 
+    Notes:
+    -- using current state --> we need use the centerline and heading of the car to determine
+    the error and where we want to go on the centerline to create some derivative
+    -- Jaden hint: do centerline on the whole length (0-105) and use all the x and y coords
+    with cubic spline to separate functions? kinda
+    -- need to make a function that takes in a x and y coordinate and returns the closest centerline position from 0 to 105
+
+    """
+    centerline_x = []
+    centerline_y = []
+
+    for i in range(0, 105):
+        xy_coord = []
+        xy_coord.append(centerline(i))
+        
+        centerline_x.append(xy_coord[0])
+        centerline_y.append(xy_coord[1])
+
+    cs = CubicSpline(centerline_x, centerline_y)
     
+
+    """
+    we are going to return two things:
+    a - how much should the car accelerate or deccelerate
+    theta - turning left or right
+    """
     return np.array([0,0])
 
 
